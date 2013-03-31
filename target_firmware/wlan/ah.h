608dbab8e3501a847f3d17ee1bd34b064a658aa1
Oleksij Rempel <linux@rempel-privat.de>
2013-03-31 18:08:32 +0200

    build fix: use ath_tx_desc in ar5416Set11nBurstDuration_20
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- target_firmware/wlan/ah.h
+++ target_firmware/wlan/ah.h
@@ -424,7 +424,7 @@ struct ath_hal
 	void      __ahdecl(*ah_clr11nAggr)(struct ath_hal *ah,
 					   struct ath_tx_desc *ds);
 	void      __ahdecl(*ah_set11nBurstDuration)(struct ath_hal *ah,
-						    struct ath_desc *ds,
+						    struct ath_tx_desc *ds,
 						    a_uint32_t burstDuration);
 	void      __ahdecl(*ah_set11nVirtualMoreFrag)(struct ath_hal *ah,
 						      struct ath_desc *ds, a_uint32_t vmf);
