f0ec3f6cefa9d843cbb65ee2ff692014c685251b
Oleksij Rempel <linux@rempel-privat.de>
2013-05-11 10:47:26 +0200

    remove ar5416GetRxDP
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- target_firmware/wlan/ah.h
+++ target_firmware/wlan/ah.h
@@ -444,7 +444,6 @@ struct ath_hal
 	u_int64_t __ahdecl(*ah_getTsf64)(struct ath_hal*);
            
 	/* Target receive Functions */
-	a_uint32_t __ahdecl(*ah_getRxDP)(struct ath_hal*);
 	void	   __ahdecl(*ah_setRxDP)(struct ath_hal*, a_uint32_t rxdp);
 	HAL_BOOL  __ahdecl(*ah_setupRxDesc)(struct ath_hal *, struct ath_rx_desc *,
 					    a_uint32_t size, a_uint32_t flags);
